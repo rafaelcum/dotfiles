eb1afa69a9d04c1e2f7201e4f0040e1e62edd4e6
rafaelcum <rafaplayrj2@gmail.com>
2022-08-22 13:15:18 -0300

    Adicionando Dec

--- digispark/DigiKeyboard.h
+++ digispark/DigiKeyboard.h
@@ -1,14 +1,29 @@
 // Using Dec
+#define KEY_ESCAPE 41
+#define KEY_BACKSPACE 42
+#define KEY_TAB 43
+#define KEY_CAPS 57
+#define KEY_PRINTSCREEN 70
+#define KEY_DELETE 76
+
+#define KEY_ARROW_RIGHT 79
+#define KEY_ARROW_LEFT 80
+#define KEY_ARROW_DOWN 81
+#define KEY_ARROW_UP 82
 
 // Using Hex
+#define KEY_ESCAPE 0x29
+#define KEY_BACKSPACE 0x2A
+#define KEY_TAB 0x2B
+#define KEY_CAPS 0x39
+#define KEY_PRINTSCREEN 0x46
+#define KEY_DELETE 0x4C
+
 #define KEY_ARROW_RIGHT 0x4F
 #define KEY_ARROW_LEFT 0x50
 #define KEY_ARROW_DOWN 0x51
 #define KEY_ARROW_UP 0x52
 
-#define KEY_TAB 0x2B
-#define KEY_CAPS 0x39
-#define KEY_BACKSPACE 0x2A
-#define KEY_ESCAPE 0x29
-#define KEY_PRINTSCREEN 0x46
-#define KEY_DELETE 0x4C
+
+
+
