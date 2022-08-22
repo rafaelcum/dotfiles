ac09068e4f3f30e19421299c5f1eee3bf9b5aedd
rafaelcum <rafaplayrj2@gmail.com>
2022-08-22 02:13:06 -0300

    Adicionado pequenos arquivos sobre digispark

---
+++ digispark/DigiKeyboard.h
@@ -0,0 +1,14 @@
+// Using Dec
+
+// Using Hex
+#define KEY_ARROW_RIGHT 0x4F
+#define KEY_ARROW_LEFT 0x50
+#define KEY_ARROW_DOWN 0x51
+#define KEY_ARROW_UP 0x52
+
+#define KEY_TAB 0x2B
+#define KEY_CAPS 0x39
+#define KEY_BACKSPACE 0x2A
+#define KEY_ESCAPE 0x29
+#define KEY_PRINTSCREEN 0x46
+#define KEY_DELETE 0x4C
